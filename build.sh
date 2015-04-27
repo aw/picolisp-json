98d8f6b7d8aa79871a1e9707a2af088cb6fae811
Alex Williams <awilliams@alexwilliams.ca>
2015-04-27 13:31:45 +0000

    Add license to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #
+# Copyright (c) 2015 Alexander Williams, Unscramble <license@unscramble.jp>
+# MIT License
+#
 # For backwards compatibility
+
+set -u
+set -e
 
 # cleanup artifacts
 rm -rf lib vendor
